330d1c0d61c09e4bb496e5ff4a92d8ff62f49a1e
gogonza95 <gonzalo.ramost@mail.udp.cl>
2016-07-02 22:27:12 -0400

    Add files via upload

--- AVL.rb
+++ AVL.rb
@@ -1,14 +1,27 @@
-class Node
-	attr_accessor :n , :left , :right
+class Contacto
+    def initialize (name, ap, tel, address, email)
+     	@name = name
+     	@ap = ap
+     	@tel = tel
+     	@address = address
+     	@email = email
+    end
+end
+
+class NodeAVL
+	attr_accessor :n , :left , :right , :d
 	def initialize n
 		@n = n
 		@left = nil
 		@right = nil
+		@d = 0
 	end
 end
 
 class AVL
 	def initialize
-		clear
+		@root = nil
 	end
+
+
 end
